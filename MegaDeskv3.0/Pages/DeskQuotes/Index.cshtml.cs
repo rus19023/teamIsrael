36e622916d76ef9e1dd4dace20a09ec6eacff3ef
Walter Taylor <walterht@byui.edu>
2022-06-10 11:07:24 -0600

    Filtering and sorting update

--- MegaDeskv3.0/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDeskv3.0/Pages/DeskQuotes/Index.cshtml.cs
@@ -19,11 +19,43 @@ namespace MegaDeskv3._0.Pages.DeskQuotes
             _context = context;
         }
 
+        public string firstNameSort { get; set; }
+        public string dateSort { get; set; }
+        public string currentFilter { get; set; }
         public IList<DeskQuote> DeskQuote { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            firstNameSort = sortOrder == "firstName_asc" ? "firstName_desc" : "firstName_asc";
+            dateSort = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+            currentFilter = searchString;
+
+            IQueryable<DeskQuote> deskQuoteIQ = from dq in _context.DeskQuote
+                                                select dq;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                deskQuoteIQ = deskQuoteIQ.Where(dq => dq.customerFirstName.Contains(searchString)
+                                                      || dq.customerLastName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "firstName_asc":
+                    deskQuoteIQ = deskQuoteIQ.OrderBy(dq => dq.customerFirstName);
+                    break;
+                case "firstName_desc":
+                    deskQuoteIQ = deskQuoteIQ.OrderByDescending(dq => dq.customerFirstName);
+                    break;
+                case "date_asc":
+                    deskQuoteIQ = deskQuoteIQ.OrderBy(dq => dq.quoteDate);
+                    break;
+                case "date_desc":
+                    deskQuoteIQ = deskQuoteIQ.OrderByDescending(dq => dq.quoteDate);
+                    break;
+            }
+
+            DeskQuote = await deskQuoteIQ.AsNoTracking().ToListAsync();
         }
     }
 }
