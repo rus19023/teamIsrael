6f86d0932ea0350e9ef0a42d6657d7c69744d4f1
rus19023 <rus19023@byui.edu>
2022-07-16 10:15:30 -0400

    push changes after tailwind issue pushed

--- SacramentMeetingPlanner/Startup.cs
+++ SacramentMeetingPlanner/Startup.cs
@@ -2,7 +2,6 @@ using SacramentMeetingPlanner.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
---
+++ SacramentMeetingPlanner/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.chstml'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
